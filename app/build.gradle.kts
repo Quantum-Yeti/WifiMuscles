0ca32a9e72cd5cab2863bc2b3087288277e01310
James Keenan <monsterbrains@protonmail.com>
2025-07-01 14:48:22 -0400

    feat: added chart style switch button for linechart/barchart
    feat(remove): removed swipe between chart styles, gestures must be custom over-ridden since gestures are enabled with chart library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "me.theoria.wifimuscles"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "2.0.2"
+        versionCode = 14
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
